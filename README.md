177c7b4810e28a51c75ba81955460a5ed005f99c
sileye ba <sileye.ba@outlook.com>
2019-10-28 21:30:33 +0100

    cosmetic fixes: update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+#This package is developped by sileye.ba@outlook.com  to extract txt
+#from pdf files. It might be used/copied without restriction. No warranty is
+#given about the way its function. Users are fully responsible of its usage.
+
 This repository contains python functions to extract text content from pdf files.
 The first developed function are based on ocr text extraction.
--- pdf_ocr_text_extraction.py
+++ pdf_ocr_text_extraction.py
@@ -1,8 +1,8 @@
 
 '''
-these function are developped by sileye.ba@outlook.com  to extract txt
-from pdf files they might be use/copied without restriction. No warranty is
-given about the way they function. Users are fully responsible of their usage.
+This package is developped by sileye.ba@outlook.com  to extract txt
+from pdf files. It might be used/copied without restriction. No warranty is
+given about the way its function. Users are fully responsible of its usage.
 '''
 
 import logging
